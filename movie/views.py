0f21e1687d7145fe8a94828f9ceb7fb6ce57f58c
AnaSofiR <asrodriguo@eafit.edu.co>
2023-08-24 09:06:09 -0500

    about y color

--- movie/views.py
+++ movie/views.py
@@ -5,4 +5,7 @@ def home(request):
    ## return HttpResponse('<h1>Welcome to home page</h1>')
     return render(request, 'home.html', {'name':'Ana Rodriguez'})
 
+def about(request):
+    return render(request, 'about.html')
+
 # Create your views here.
